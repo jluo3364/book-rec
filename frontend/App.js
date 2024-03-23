47552abad2244f2b07b55bf4fe2d59496702a264
nataliechow <nchow234@gmail.com>
2024-03-23 15:01:47 -0400

    fix more merge issues

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Login from './Login'; // Adjust the path according to your file structure
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login />
-=======
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { colors } from "./colors";
-export default function App() {
-  return (
-    <View style={styles.container}>
       <Text>Bookup</Text>
+      <Login />
       <StatusBar style="auto" />
->>>>>>> 61c34ccaf1c1fbf007158bfd19f35ed50085a21b
     </View>
   );
 }
