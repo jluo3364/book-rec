89fc87c90ab48d4215a8938837c77180fc84b675
jluo3364 <77076660+jluo3364@users.noreply.github.com>
2024-03-24 11:30:25 -0400

    camera img to reponsepage

--- frontend/Camera.js
+++ frontend/Camera.js
@@ -1,19 +1,25 @@
 import { Camera, CameraType } from "expo-camera";
 import { useState, useRef } from "react"; // Import useRef
+// import { useEffect } from "react";
 import {
   Button,
   StyleSheet,
   Text,
   Image,
   TouchableOpacity,
+  Pressable,
   View,
 } from "react-native";
+import { colors } from "./colors";
+import { useNavigation } from "@react-navigation/native";
 
 export default function App() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const cameraRef = useRef(null);
   const [capturedPhotoUri, setCapturedPhotoUri] = useState(null);
+  const [responseData, setResponseData] = useState(null);
+  const navigation = useNavigation();
 
   if (!permission) {
     // Camera permissions are still loading
@@ -53,7 +59,7 @@ export default function App() {
       formData.append("photo", photo.uri);
 
       // Send the photo to the backend using Fetch API
-      const response = await fetch("http://localhost:8086/upload", {
+      const response = await fetch("http://localhost:8081/upload", {
         method: "POST",
         body: formData,
         headers: {
@@ -65,11 +71,23 @@ export default function App() {
         throw new Error("Failed to upload photo");
       }
       console.log("Photo captured:", photo);
+
+      // Get the response from the backend
+      const getDataResponse = await fetch(
+        "http://localhost:8081/generate-content"
+      );
+      if (!getDataResponse.ok) {
+        throw new Error("Failed to get data from gemini api");
+      }
+      const data = await getDataResponse.json();
+      setResponseData(data);
     } else {
       console.error("Camera reference is not available.");
     }
   };
-
+  // useEffect(() => {
+  //   console.log("api generated response:", responseData);
+  // }, [responseData]);
   return (
     <View style={styles.container}>
       <Camera ref={cameraRef} style={styles.camera} type={type}>
@@ -79,15 +97,27 @@ export default function App() {
             onPress={toggleCameraType}
             style={styles.button}
           />
-          <Button title="Take Picture" onPress={takePicture} />
+          <Button
+            title="Take Picture"
+            onPress={takePicture}
+            style={styles.button}
+          />
         </View>
       </Camera>
       {capturedPhotoUri && (
         <View>
-          <Text style={styles.text}>Captured photo:</Text>
-          <TouchableOpacity onPress={() => setCapturedPhotoUri(null)}>
-            <Text style={styles.text}>Close</Text>
-          </TouchableOpacity>
+          <Text style={styles.normalText}>captured photo:</Text>
+          <Pressable
+            onPress={() =>
+              navigation.navigate("ResponsePage", {
+                bookImg: capturedPhotoUri,
+                info: responseData,
+              })
+            }
+            style={styles.closebutton}
+          >
+            <Text style={styles.text}>use this photo</Text>
+          </Pressable>
           <Image
             source={{ uri: capturedPhotoUri }}
             style={{ width: 300, height: 300 }}
@@ -101,13 +131,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.dustyred,
     alignItems: "center",
     justifyContent: "center",
   },
   camera: {
-    flex: 1,
+    flex: 0.8,
     width: "100%",
+    // height:""
+  },
+  closebutton: {
+    backgroundColor: colors.tan,
+    borderRadius: 12,
+    verticalMargin: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonContainer: {
     flexDirection: "row",
@@ -118,10 +156,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignSelf: "flex-end",
     alignItems: "center",
+    backgroundColor: colors.tan,
   },
   text: {
     fontSize: 18,
-    color: "black",
+    color: colors.dustyred,
+    marginTop: 10,
+  },
+  normalText: {
+    fontSize: 18,
+    color: colors.tan,
     marginTop: 10,
   },
 });
